e21f0a216fcbe6c287339acd2b69b2755baf7a0a
vamsi kiran <vamsikiran@vamsis-MacBook-Air.local>
2025-03-20 21:38:23 +0530

    fix bug in flight updates

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -53,7 +53,7 @@ class FlightRepository extends CrudRepository
     {
          await db.sequelize.query(addRowLockOnFlights(flightId));
          const flight=await Flight.findByPk(flightId);
-        if(parseInt(dec))
+        if(+dec)
         {
             const response=await flight.decrement('totalSeats',{by: seats});
             return response;
